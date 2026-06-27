5566d989ab5e031d3fe302d380de390a2d766e9c
yjade <954926802@qq.com>
2026-06-27 21:12:16 +0800

    初始化

---
+++ src/test/java/com/redbook/redbook/RedbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redbook.redbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
